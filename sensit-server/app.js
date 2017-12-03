4743ece5d208ab27a15ba30e6539dac79803f579
darajava <darajavaherian@gmail.com>
2017-12-03 15:41:24 +1100

    get basic JWT auth working

--- sensit-server/app.js
+++ sensit-server/app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var JWTStrategy = require('passport-jwt').Strategy;
+var ExtractJwt = require('passport-jwt').ExtractJwt;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,11 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
-app.use(require('express-session')({
-    secret: 'keyboard cat',
-    resave: false,
-    saveUninitialized: false
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -38,7 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   next();
 });
 
@@ -47,6 +44,32 @@ app.use('/', routes);
 // passport config
 var User = require('./models/user');
 passport.use(new LocalStrategy(User.authenticate()));
+
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'tokenSecret';
+// opts.authScheme = 'JWT';
+
+passport.use(new JWTStrategy(opts, function(jwt_payload, done) {
+    console.log(jwt_payload);
+    User.findOne({username: jwt_payload.username}, function(err, user) {
+        if (err) {
+            console.log('1');
+            console.log(err);
+            return done(err, false);
+        }
+
+        if (user) {
+            console.log('2');
+            console.log(user);
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
--- sensit-server/routes/index.js
+++ sensit-server/routes/index.js
@@ -5,14 +5,14 @@ var express = require('express');
 var passport = require('passport');
 var User = require('../models/user');
 var router = express.Router();
-
+var bCrypt = require('bcrypt-nodejs');
+var jwt = require('jsonwebtoken');
 
 router.get('/', function (req, res) {
     res.render('index', { user : req.user });
 });
 
 router.post('/register', function(req, res) {
-    console.log(req.body);
     User.register(new User({ username : req.body.username }), req.body.password, function(err, user) {
         if (err) {
             res.status(401);
@@ -30,8 +30,31 @@ router.get('/login', function(req, res) {
     res.render('login', { user : req.user });
 });
 
-router.post('/login', passport.authenticate('local'), function(req, res) {
-    res.redirect('/');
+router.post('/login', function(req, res, next) {
+
+  // generate the authenticate method and pass the req/res
+  passport.authenticate('local', function(err, user, info) {
+    if (err) {
+        return next(err);
+    }
+
+    if (!user) {
+        res.status(401);
+        return res.send({ success: false, error: 'User not found' });
+    }
+
+    // XXX: update secret
+    var token = jwt.sign({ username: req.body.username}, 'tokenSecret');
+
+    res.status(200);
+    return res.json({success: true, token: token});
+
+  })(req, res, next);
+
+});
+
+router.post("/chats", passport.authenticate('jwt'), function(req, res){
+  res.json("Success! You can not see this without a token");
 });
 
 router.get('/logout', function(req, res) {
